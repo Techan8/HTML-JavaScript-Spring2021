9ef4112f6d08a8dc3deeea018fb22a80b2d88642
Techan8 <mboaventura@email.neit.edu>
2021-05-19 22:20:32 -0400

    assignment 07

--- Week7/Day 2/AsteroidAvoider/AsteroidAvoider.js
+++ Week7/Day 2/AsteroidAvoider/AsteroidAvoider.js
@@ -1,28 +1,31 @@
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
-var timer =  requestAnimationFrame(main)
+var timer = requestAnimationFrame(main)
 var ship
 //var to setup number of asteroids
 var numAsteroids = 20
 //array for asteroids
 var asteroids = new Array()
+var gameOver = true
+var gameStates = []
+var currentState = 0
 
 //Create an instance of the PlayerShip
-ship =  new PlayerShip()
+ship = new PlayerShip()
 
-function randomRange(high, low){
-    return Math.random() * (high-low) + low
+function randomRange(high, low) {
+    return Math.random() * (high - low) + low
 }
 
 //Constructor Function for Asteroid Class
-function Asteroid(){
-    this.radius = randomRange(15,2)
+function Asteroid() {
+    this.radius = randomRange(15, 2)
     this.x = randomRange(canvas.width - this.radius, this.radius)
     this.y = randomRange(canvas.height - this.radius, this.radius) - canvas.height
-    this.vy = randomRange(10,5)
+    this.vy = randomRange(10, 5)
     this.color = "white"
 
-    this.drawAsteroid = function(){
+    this.drawAsteroid = function () {
         ctx.save
         ctx.beginPath()
         ctx.fillStyle = this.color
@@ -36,7 +39,7 @@ function Asteroid(){
 
 //For Loop to create the instances of Asteroids
 
-for(var i = 0; i < numAsteroids; i++){
+for (var i = 0; i < numAsteroids; i++) {
     asteroids[i] = new Asteroid()
 }
 
@@ -44,40 +47,63 @@ for(var i = 0; i < numAsteroids; i++){
 document.addEventListener("keydown", pressKeyDown)
 document.addEventListener("keyup", pressKeyUp)
 
-function pressKeyDown(e){
-    if(e.keyCode == 87){
-        ship.up = true
+function pressKeyDown(e) {
+    if (!gameOver) {
+        if (e.keyCode == 87) {
+            ship.up = true
+        }
+        if (e.keyCode == 65) {
+            ship.left = true
+        }
+        if (e.keyCode == 68) {
+            ship.right = true
+        }
+        if (e.keyCode == 83) {
+            ship.down = true
+        }
     }
-    if(e.keyCode == 65){
-        ship.left = true
-    }
-    if(e.keyCode == 68){
-        ship.right = true
-    }
-    if(e.keyCode == 83){
-        ship.down = true
+
+    if (gameOver) {
+
+        //checking for spacebar
+        if (e.keyCode == 32) {
+            if (currentState == 2) {
+                //game over screen
+                currentState = 0
+                main()
+            }
+            else {
+                currentState = 1
+                gameOver = false
+                main()
+                console.log("space")
+            }
+        }
     }
 }
 
-function pressKeyUp(e){
-    if(e.keyCode == 87){
-        ship.up = false
-    }
-    if(e.keyCode == 65){
-        ship.left = false
-    }
-    if(e.keyCode == 68){
-        ship.right = false
-    }
-    if(e.keyCode == 83){
-        ship.down = false
+
+function pressKeyUp(e) {
+    if (!gameOver) {
+        if (e.keyCode == 87) {
+            ship.up = false
+        }
+        if (e.keyCode == 65) {
+            ship.left = false
+        }
+        if (e.keyCode == 68) {
+            ship.right = false
+        }
+        if (e.keyCode == 83) {
+            ship.down = false
+        }
     }
 }
 
 //Constructor function
-function PlayerShip(){
-    this.x = canvas.width/2
-    this.y = canvas.height/2
+function PlayerShip() {
+    this.x = canvas.width / 2
+    this.y = canvas.height / 2
     this.w = 20
     this.h = 20
     this.vx = 0 //for velocity
@@ -88,24 +114,24 @@ function PlayerShip(){
     this.right = false
     this.flamelength = 30
 
-    this.drawShip = function(){
+    this.drawShip = function () {
         ctx.save() //save previous context until restore
         ctx.translate(this.x, this.y)
-        if(this.up || this.left || this.right){
+        if (this.up || this.left || this.right) {
             ctx.save()
             //Changes the drawing values to animate the flame
-            if(this.flamelength == 30){
+            if (this.flamelength == 30) {
                 this.flamelength = 20
                 ctx.fillStyle = "yellow"
-            }else{
+            } else {
                 this.flamelength = 30
                 ctx.fillStyle = "orange"
             }
             ctx.beginPath()
-            
+
             ctx.moveTo(0, this.flamelength)
-            ctx.lineTo(5,5)
-            ctx.lineTo(-5,5)
+            ctx.lineTo(5, 5)
+            ctx.lineTo(-5, 5)
             ctx.lineTo(0, this.flamelength)
             ctx.closePath()
             ctx.fill()
@@ -124,71 +150,122 @@ function PlayerShip(){
 
     }
 
-    this.move = function(){
+    this.move = function () {
         this.x += this.vx
         this.y += this.vy
 
         //setting up the boundaries
         //bottom boundary of screen
-        if(this.y > canvas.height - this.h/2){
-            this.y = canvas.height - this.h/2
+        if (this.y > canvas.height - this.h / 2) {
+            this.y = canvas.height - this.h / 2
             this.vy = 0
         }
 
         //top boundary of screen
-        if(this.y < this.h/2){
-            this.y =  this.h/2
+        if (this.y < this.h / 2) {
+            this.y = this.h / 2
             this.vy = 0
         }
 
         //right boundary of screen
-        if(this.x > canvas.width - this.w/2){
-            this.x = canvas.width - this.w/2
+        if (this.x > canvas.width - this.w / 2) {
+            this.x = canvas.width - this.w / 2
             this.vx = 0
         }
 
         //left boundary of screen
-        if(this.x < this.w/2){
-            this.x =  this.w/2
+        if (this.x < this.w / 2) {
+            this.x = this.w / 2
             this.vx = 0
         }
     }
-    
+
 }
 
-function main(){
-    //clear canvas 
-    ctx.clearRect(0,0, canvas.width, canvas.height)
+gameStates[0] = function () {
+    ctx.save()
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "blue"
+    ctx.textAlign = "center"
+    ctx.fillText("Asteroid Avoider", canvas.width / 2, canvas.height / 2 - 30)
+    ctx.font = "15px Arial"
+    ctx.fillStyle = "blue"
+    ctx.textAlign = "center"
+    ctx.fillText("Press Space To Start", canvas.width / 2, canvas.height / 2 + 20)
+    ctx.restore()
+}
 
+gameStates[1] = function () {
     //update player movement
     //vertical movement
-    if(ship.up){
+    if (ship.up) {
         ship.vy = -10
-    }else{
+    } else {
         ship.vy = 3
     }
 
     //horizontal movement
-    if(ship.left){
+    if (ship.left) {
         ship.vx = -3
-    }else if(ship.right){
+    } else if (ship.right) {
         ship.vx = 3
-    }else{
+    } else {
         ship.vx = 0
     }
+    //Loops through all asteroids and can check their position
+    for (var i = 0; i < asteroids.length; i++) {
+        var dX = ship.x - asteroids[i].x
+        var dY = ship.y - asteroids[i].y
+        var distance = Math.sqrt((dX * dX) + (dY * dY))
 
-    for(var i = 0; i < asteroids.length; i++){
+        if (detectCollision(distance, (ship.h / 2 + asteroids[i].radius))) {
+            console.log("hit asteroid")
+            gameOver = true
+            currentState = 2
+            main()
 
-        if(asteroids[i].y > canvas.height + asteroids[i].radius){
+        }
+
+        if (asteroids[i].y > canvas.height + asteroids[i].radius) {
             asteroids[i].x = randomRange(canvas.width - asteroids[i].radius, asteroids[i].radius)
             asteroids[i].y = randomRange(canvas.height - asteroids[i].radius, asteroids[i].radius) - canvas.height
         }
-        asteroids[i].y += asteroids[i].vy
-        asteroids[i].drawAsteroid()
+        if (!gameOver) {
+            asteroids[i].y += asteroids[i].vy
+            asteroids[i].drawAsteroid()
+        }
     }
-    
-    ship.move()
-    ship.drawShip()
+    if (!gameOver) {
+        ship.move()
+        ship.drawShip()
+    }
+}
 
-    timer = requestAnimationFrame(main)
+gameStates[2] = function () {
+    ctx.save()
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "red"
+    ctx.textAlign = "center"
+    ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2 - 30)
+    ctx.font = "15px Arial"
+    ctx.fillStyle = "red"
+    ctx.textAlign = "center"
+    ctx.fillText("Press Space To Play Again", canvas.width / 2, canvas.height / 2 + 20)
+    ctx.restore()
+}
+
+function main() {
+    //clear canvas 
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    gameStates[currentState]()
+
+    if (!gameOver) {
+        timer = requestAnimationFrame(main)
+    }
+
+}
+
+function detectCollision(distance, calcDistance) {
+    return distance < calcDistance
 }
---
+++ w7/Boaventura_07/script.js
@@ -0,0 +1,408 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+var timer = requestAnimationFrame(main)
+
+var gameStates = []
+var currentState = 0
+
+option.style.display = "none"
+
+var rps = []
+rps[0] = "rock"
+rps[1] = "paper"
+rps[2] = "scissors"
+
+var pScore = 0
+var cScore = 0
+
+var rock = new Image()
+rock.src = 'images/rock.png'
+var rock2 = new Image()
+rock2.src = 'images/rock2.png'
+var paper = new Image()
+paper.src = 'images/paper.png'
+var paper2 = new Image()
+paper2.src = 'images/paper2.png'
+var scissors = new Image()
+scissors.src = 'images/scissors.png'
+var scissors2 = new Image()
+scissors2.src = 'images/scissors2.png'
+var computer = new Image()
+computer.src = 'images/computer.png'
+
+var gameOver = true
+
+
+document.addEventListener("keydown", keyPressDown)
+document.addEventListener("keyup", keyPressUp)
+
+var btn = document.querySelectorAll('button')
+btn[0].addEventListener("click", function (e) { game(0) })
+btn[1].addEventListener("click", function (e) { game(1) })
+btn[2].addEventListener("click", function (e) { game(2) })
+
+
+function keyPressDown(e) {
+    console.log(e.keyCode)
+}
+
+function keyPressUp(e) {
+    console.log(e.keyCode)
+    if (gameOver == true) {
+        if (e.keyCode == 32) {
+            //this is space bar
+            changeState()
+        }
+    }
+}
+
+function changeState() {
+    if (currentState >= gameStates.length - 1) {
+        currentState = 0
+    }
+    else {
+        currentState++
+    }
+
+}
+
+function game(pChoice) {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    var cChoice = Math.floor(Math.random() * 2.99)
+
+    switch (pChoice) {
+        case 0:
+            console.log(cChoice)
+            if (cChoice == 0) {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Rock-Rock, It's a Tie", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Rock-Rock, It's a Tie", canvas.width / 2, canvas.height / 2 + 15)
+                console.log("here")
+                ctx.drawImage(rock, 450, 500, 100, 80)
+                ctx.drawImage(rock2, 450, 650, 100, 80)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+            }
+            else if (cChoice == 1) {
+
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Rock-Paper, Computer Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Rock-Paper, Computer Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(paper, 450, 500, 100, 80)
+                ctx.drawImage(rock2, 450, 650, 100, 80)
+                cScore++
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+
+
+            }
+            else {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Rock-Scissors, Player Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Rock-Scissors, Player Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(scissors, 450, 500, 100, 80)
+                ctx.drawImage(rock2, 450, 650, 100, 80)
+                pScore++
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+
+            }
+            break;
+        case 1:
+            if (cChoice == 0) {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Paper-Rock, Player Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Paper-Rock, Player Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(rock, 450, 500, 100, 80)
+                ctx.drawImage(paper2, 450, 650, 100, 80)
+                pScore++
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+            }
+            else if (cChoice == 1) {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Paper-Paper, It's a Tie", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Paper-Paper, It's a Tie", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(paper, 450, 500, 100, 80)
+                ctx.drawImage(paper2, 450, 650, 100, 80)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+
+
+            }
+            else {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Paper-Scissors, Computer Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Paper-Scissors, Computer Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(scissors, 450, 500, 100, 80)
+                ctx.drawImage(paper2, 450, 650, 100, 80)
+                cScore++
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+
+            }
+            break;
+        case 2:
+            if (cChoice == 0) {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Scissors-Rock, Computer Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Scissors-Rock, Computer Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(rock, 450, 500, 100, 80)
+                ctx.drawImage(scissors2, 450, 650, 100, 80)
+                cScore++
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+            }
+            else if (cChoice == 1) {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Scissors-Paper, Player Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Scissors-Paper, Player Wins", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(paper, 450, 500, 100, 80)
+                ctx.drawImage(scissors2, 450, 650, 100, 80)
+                pScore++
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+
+            }
+            else {
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "50px Reggae One"
+                ctx.fillText("RPS GAME", 500, 75)
+                ctx.strokeText("RPS GAME", 500, 75)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.textAlign = "center"
+                ctx.fillText("Scissors-Scissors, It's a Tie", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.strokeText("Scissors-Scissors, It's a Tie", canvas.width / 2, canvas.height / 2 + 15)
+                ctx.drawImage(scissors, 450, 500, 100, 80)
+                ctx.drawImage(scissors2, 450, 650, 100, 80)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Player Score: " + pScore, 250, 230)
+                ctx.strokeText("Player Score: " + pScore, 250, 230)
+                ctx.fillStyle = "#0ff"
+                ctx.strokeStyle = "black"
+                ctx.font = "35px Reggae One"
+                ctx.fillText("Computer Score: " + cScore, 720, 230)
+                ctx.strokeText("Computer Score: " + cScore, 720, 230)
+            }
+            break;
+    }
+
+}
+
+
+gameStates[0] = function () {
+
+    ctx.fillStyle = "#0ff"
+    ctx.strokeStyle = "black"
+    ctx.lineWidth = 2
+    ctx.shadowColor = "#0ff"
+    ctx.shadowOffsetX = 0
+    ctx.shadowOffsetY = 0
+    ctx.shadowBlur = 10
+    ctx.textAlign = "center"
+    ctx.font = "50px Reggae One"
+    ctx.fillText("Welcome To The RPS Game", canvas.width / 2, canvas.height / 2 - 50)
+    ctx.strokeText("Welcome To The RPS Game", canvas.width / 2, canvas.height / 2 - 50)
+    ctx.font = "30px Reggae One"
+    ctx.fillText("Press Spacebar To Start", canvas.width / 2, canvas.height / 2 + 25)
+    ctx.strokeText("Press Spacebar To Start", canvas.width / 2, canvas.height / 2 + 25)
+
+}
+
+gameStates[1] = function () {
+    
+    gameOver = false
+    option.style.display = "block"
+    //title
+    ctx.fillStyle = "#0ff"
+    ctx.strokeStyle = "black"
+    ctx.font = "50px Reggae One"
+    ctx.fillText("RPS GAME", 500, 75)
+    ctx.strokeText("RPS GAME", 500, 75)
+    //p&cpu score
+    ctx.fillStyle = "#0ff"
+    ctx.strokeStyle = "black"
+    ctx.font = "35px Reggae One"
+    ctx.fillText("Player Score: " + pScore, 250, 230)
+    ctx.strokeText("Player Score: " + pScore, 250, 230)
+    ctx.fillStyle = "#0ff"
+    ctx.strokeStyle = "black"
+    ctx.font = "35px Reggae One"
+    ctx.fillText("Computer Score: " + cScore, 720, 230)
+    ctx.strokeText("Computer Score: " + cScore, 720, 230)
+    //images
+    ctx.drawImage(computer, 435, 500, 100, 70)
+    ctx.drawImage(rock, 200, 650, 70, 60)
+    ctx.drawImage(paper, 450, 650, 80, 60)
+    ctx.drawImage(scissors, 700, 650, 70, 60)
+    ctx.drawImage(pChoice, 200, 650, 70, 60)
+    
+    if(pScore || cScore >= 5){
+        pScore || cScore == 0
+        gameOver = true
+        currentState++
+    }
+
+}
+
+gameStates[2] = function() {
+    ctx.fillStyle = "#0ff"
+    ctx.strokeStyle = "black"
+    ctx.lineWidth = 2
+    ctx.shadowColor = "#0ff"
+    ctx.shadowOffsetX = 0
+    ctx.shadowOffsetY = 0
+    ctx.shadowBlur = 10
+    ctx.textAlign = "center"
+    ctx.font = "50px Reggae One"
+    ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2 - 50)
+    ctx.strokeText("Game Over", canvas.width / 2, canvas.height / 2 - 50)
+    ctx.font = "30px Reggae One"
+    ctx.fillText("Press Spacebar To Play Again", canvas.width / 2, canvas.height / 2 + 25)
+    ctx.strokeText("Press Spacebar To Play Again", canvas.width / 2, canvas.height / 2 + 25)
+}
+function main() {
+    //clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    gameStates[currentState]()
+    
+
+
+    //call main function
+    timer = requestAnimationFrame(main)
+}
