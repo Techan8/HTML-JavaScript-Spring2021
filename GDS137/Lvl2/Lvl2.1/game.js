0746149d79111d8634722a19d956aa859bc83d25
Your Name <you@example.com>
2021-10-16 01:20:08 -0400

    link 2.1 and 2.2

---
+++ GDS137/Lvl2/Lvl2.1/game.js
@@ -0,0 +1,24 @@
+var canvas = document.querySelector('canvas')
+var ctx = canvas.getContext('2d')
+
+var timer = setInterval(animate, interval);
+var interval = 1000/60; //1000 ms or 1 second / FPS
+var paddle = new GameObject()
+
+//paddle position
+paddle.x = 25
+paddle.y = 400
+
+//paddle size
+paddle.w = 20
+paddle.h = 200
+
+function animate()
+{
+	//Erase the Screen
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+
+
+	//Update the Screen
+	paddle.drawRect();
+}
---
+++ GDS137/Lvl2/Lvl2.2/game.js
@@ -0,0 +1,34 @@
+var canvas = document.querySelector('canvas')
+var ctx = canvas.getContext('2d')
+
+var timer = setInterval(animate, interval);
+var interval = 1000/60; //1000 ms or 1 second / FPS
+var paddle = new GameObject()
+
+//paddle position
+paddle.x = 25
+paddle.y = 400
+
+//paddle size
+paddle.w = 20
+paddle.h = 200
+
+function animate()
+{
+	//Erase the Screen
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+	//Move the Player to the right
+	if(s)
+	{
+		console.log("Moving up");
+		paddle.y += 2;
+	}
+	if(w)
+	{
+		console.log("Moving down");
+		paddle.y += -2;
+	}
+	
+	//Update the Screen
+	paddle.drawRect();
+}
