6bd948d7897219fa801ab0b9a75172befd99f6d5
Your Name <you@example.com>
2021-10-18 14:19:34 -0400

    2.7

--- GDS137/Lvl2/Lvl2.7/game.js
+++ GDS137/Lvl2/Lvl2.7/game.js
@@ -6,6 +6,12 @@ var interval = 1000/60; //1000 ms or 1 second / FPS
 var collisionScore = 0
 
 var ball = new GameObject()
+//Ball's size
+ball.width = 80
+ball.height = 80
+//Ball's velocity
+ball.vx = -2
+ball.vy = 0
 var paddle = new GameObject()
 
 //This is used to stop the player from moving through obstacles.
@@ -16,8 +22,8 @@ paddle.x = 5
 paddle.y = canvas.height/2
 
 //paddle size
-paddle.w = 20
-paddle.h = 200
+paddle.width = 20
+paddle.height = 200
 
 function animate()
 {
@@ -28,29 +34,36 @@ function animate()
 	{
 		console.log("Moving up");
 		paddle.y += 2;
-		if(paddle.y + paddle.h > canvas.height + paddle.h/2)
-		{
-			paddle.y = canvas.height - paddle.h /2
-		}
+		
 
 	}
 	if(w)
 	{
 		console.log("Moving down");
 		paddle.y += -2;
-		if( paddle.y < 0 + paddle.h/2 )
-		{
-			paddle.y = 0 + paddle.h/2
-		}
+		
 	}
 
+	//paddle screen bounderies
+	if(paddle.y + paddle.height > canvas.height + paddle.height/2)
+	{
+		paddle.y = canvas.height - paddle.height /2
+	}
+
+	if( paddle.y < 0 + paddle.height/2 )
+	{
+		paddle.y = 0 + paddle.height/2
+	}
+	
 	//Move the Ball
 	ball.x += ball.vx
 	ball.y += ball.vy
 
-    if(ball.x > canvas.width - ball.radius )//|| ball.x - ball.radius <0)
+    if(ball.x > canvas.width - ball.width/2 )//|| ball.x - ball.width/2 <0)
     {
+		ball.x = canvas.width - ball.width/2 
         ball.vx = -ball.vx;
+		//increase velocity at collision
 		/*if(ball.vx < 0)
 		{
 			--ball.vx
@@ -65,16 +78,18 @@ function animate()
 		
     }
 	//lose condition - resets the ball to the middle of screen if passes L edge
-	if(ball.x -ball.radius < 0)
+	if(ball.x <  0 - ball.width/2)
 
 	{
+		
 		ball.x = canvas.width/2
 		ball.y = canvas.height/2
 		
 	}
-	/*if(ball.y > canvas.height - ball.radius || ball.y - ball.radius <0)
+	if(ball.y > canvas.height - ball.width/2 )
     {
-        ball.vy = -ball.vy ;*/
+		ball.y = canvas.height - ball.width/2
+        ball.vy = -ball.vy ;
 		/*if(ball.vy < 0)
 		{
 			--ball.vy
@@ -84,48 +99,48 @@ function animate()
 			++ball.vy
 		}*/
 
-		/*collisionScore ++ //Increase collision score
+		collisionScore ++ //Increase collision score
 		ball.color = "cyan" //Change the color 
 		
 		
-    }*/
-	//collision detection
-	if (ball.hitTestObject(paddle)) {
-		//change color
-		ball.color = "yellow";
-		ball.vx = -ball.vx			// bounce against paddle
-	}
-	else 
+    }
+
+	if (ball.y - ball.width/2 <0)
 	{
+		ball.y = 0 + ball.width/2 
+		ball.vy = -ball.vy ;
+	}
+	//collision detection
+	
+	
+
+	if (ball.hitTestObject(paddle)) 
+	{
+		ball.x = paddle.x + paddle.width/2 + ball.width/2
+		//changes color of the ball
+		ball.color = "yellow"
+		//changes the direction of the ball
+		ball.vx = -ball.vx
+
+		//changes ball's velocity direction
+		if(ball.y < paddle.y - paddle.y/6)
+     	{
+       		ball.vy = -ball.force
+     	}
+		else if(ball.y > paddle.y + paddle.height/6)
+		{
+			ball.vy = ball.force
+		}
+		
+	}
+	
+	else {
 		ball.color = ball.color;
 		
 	}
 
-	//Shows Bounding Boxes
-	if (ball.hitTestObject(paddle)) {
-		//draw bounding boxes
-		ctx.strokeRect(ball.x - ball.width / 2, ball.y - ball.height / 2, ball.width, ball.height)
-		ctx.strokeRect(paddle.x - paddle.width / 2, paddle.y - paddle.height / 2, paddle.width, paddle.height)
-		
-	}
-
-	//Demonstrates how often collisions take place
-	if (ball.hitTestObject(paddle)) {
-		console.log("colliding");
-	}
-
-	//Impede movement
-	if (ball.hitTestObject(paddle)) {
-		paddle.x = prevX;
-		console.log("colliding");
-	}
-	else {
-		prevX = paddle.x;
-	}
-	
 	//Update the Screen
 	ball.drawCircle();
-	
-	//Update the Screen
 	paddle.drawRect();
+
 }
